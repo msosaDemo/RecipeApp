690b0e1d882e9cb0ace0a0fee9cac357dcc19b2a
Mauricio Sosa <msosa@imobile3.com>
2024-10-09 13:44:41 -0400

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Mauricio Pimienta on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
